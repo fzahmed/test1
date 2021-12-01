63a425c684f1052aea7c3289060bc2217dc2178d
fzahmed <dhaka.faisal@gmail.com>
2021-12-01 14:35:38 -0600

    test with laboni

---
+++ src/main/java/Learn/Zayan.java
@@ -0,0 +1,16 @@
+package Learn;
+
+public class Zayan {
+	public static void main(String[] args) {
+	
+		
+		System.out.println("kkfxkqjegdkqueg");
+		System.out.println("hello");
+		
+		
+}
+	
+	}
+	
+	
+
--- src/main/java/Learn/lala.java
+++ src/main/java/Learn/lala.java
@@ -1,14 +1,11 @@
 package Learn;
 
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
